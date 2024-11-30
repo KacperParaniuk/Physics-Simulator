0ae9bf7bb15d88ed7d2d0ce536d882816ae5f118
KacperParaniuk <kacparaniuk@gmail.com>
2024-11-29 22:52:05 -0600

    Create Test.

---
+++ Test.py
@@ -0,0 +1,14 @@
+sum = 0
+
+print("You are going to add two numbers")
+num1 = int(input("What is the first number?"))
+num2 = int(input("What is the second number?"))
+
+print(num1+num2)
+
+for x in range(20):
+    sum+=x
+    print(x)
+
+print(sum)
+
